0cf02510409d142f1dc43abcf9ec1d067383aba5
圈圈 <vvvvvvii@yahoo.com.tw>
2021-03-15 18:44:39 +0800

    完成上方新增套票

--- all.js
+++ all.js
@@ -50,40 +50,85 @@ let data = [
     }
 ]
 let ticketList = document.querySelector('#ticketList');
-//console.log(ticketList);
-let str = "";
-data.forEach(item=>{
-    if(typeof(item.leftNum)=="number"){
-        item.leftNum = `剩下最後 ${item.leftNum}  組`;
+let addTicketSubmit = document.querySelector('#addTicketSubmit');
+let addTicketName = document.querySelector('#addTicketName');
+let addImageURL = document.querySelector('#addImageURL');
+let addRegion = document.querySelector('#addRegion');
+let addScore = document.querySelector('#addScore');
+let addTicketInfo = document.querySelector('#addTicketInfo');
+let addPrice = document.querySelector('#addPrice');
+
+//event listener
+document.addEventListener('DOMContentLoaded',showTicket)
+addTicketSubmit.addEventListener('click',addTicket); //本來想用submit事件，但沒辦法執行成功（我有加e.preventDefault呀？）還請助教解惑！
+
+//function
+function addTicket(e){
+    e.preventDefault();
+    if(addTicketName.value==""){
+        alert('請填寫套票名稱');
+    }else if(addImageURL.value==""){
+        alert('請填寫套票網址');
+    }else if(addRegion.value==""){
+        alert('請填寫景點地區');
+    }else if(addScore.value==""){
+        alert('請填寫套票星級');
+    }else if(addTicketInfo.value==""){
+        alert('請填寫套票描述');
+    }else if(addLeftNum.value==""){
+        alert('請填寫套票組數');
+    }else if(addPrice.value==""){
+        alert('請填寫套票金額');
+    }else{
+        data.push({
+            ticketName: addTicketName.value,
+            imageURL: addImageURL.value,
+            region: addRegion.value,
+            score: parseInt(addScore.value),
+            ticketInfo: addTicketInfo.value,
+            leftNum: parseInt(addLeftNum.value),
+            price: parseInt(addPrice.value)
+        });
+        showTicket();
     }
-    str+=`<li class="col-4 mb-6">
-    <div class="card position-relative shadow bg-white rounded">
-        <div class="info-tag-lg position-absolute">
-            <h3 class="h5">${item.region}</h3>
-        </div>
-        <img src=${item.imageURL} class="card-img-top" alt="">
-        <div class="info-tag-sm position-absolute">
-            <p>${item.score}</p>
-        </div>
-        <div class="card-body">
-            <h5 class="card-title card-title-border-bottom mb-4">${item.ticketName}</h5>
-            <p class="card-text mb-8">
-                ${item.ticketInfo}    
-            </p>
-            <div class="d-flex justify-content-between align-items-center text-primary">
-                <p class="d-flex align-items-center">
-                    <span class="material-icons mr-1">
-                        error
-                    </span>
-                    ${item.leftNum}
+    console.log(data);
+}
+function showTicket(){
+    let str = "";
+    data.forEach(item=>{
+        if(typeof(item.leftNum)=="number"){
+            item.leftNum = `剩下最後 ${item.leftNum}  組`;
+        }
+        str+=`<li class="col-4 mb-6">
+        <div class="card position-relative shadow bg-white rounded">
+            <div class="info-tag-lg position-absolute">
+                <h3 class="h5">${item.region}</h3>
+            </div>
+            <img src=${item.imageURL} class="card-img-top" alt="">
+            <div class="info-tag-sm position-absolute">
+                <p>${item.score}</p>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title card-title-border-bottom mb-4">${item.ticketName}</h5>
+                <p class="card-text mb-8">
+                    ${item.ticketInfo}    
                 </p>
-                <p class="h2 d-flex align-items-center mr-1">
-                    <span class="h6">TWD</span>
-                    ${item.price} 
-                </p>
+                <div class="d-flex justify-content-between align-items-center text-primary">
+                    <p class="d-flex align-items-center">
+                        <span class="material-icons mr-1">
+                            error
+                        </span>
+                        ${item.leftNum}
+                    </p>
+                    <p class="h2 d-flex align-items-center mr-1">
+                        <span class="h6">TWD</span>
+                        ${item.price} 
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-</li>`
-})
-ticketList.innerHTML = str;
+    </li>`
+    })
+    ticketList.innerHTML = str;
+}
+
